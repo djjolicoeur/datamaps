68d7568adeca8a3ad4609c00e5a2c9aa8ecc623a
djjolicoeur <djjolicoeur@gmail.com>
2016-03-26 15:50:54 -0400

    filter only once and partition on creation.  README updates

--- src/datamaps/core.clj
+++ src/datamaps/core.clj
@@ -8,27 +8,52 @@
 ;; different semantics and not lose the ability to rebuild the
 ;; entity/map from the facts (see entity and datums->map).
 
-(defn filter-meta
+;; Attribute Types -- namespaced keywords
+
+(def coll-type ::coll)
+(def val-type  ::val)
+(def ref-type  ::ref)
+
+(def attr-types #{coll-type val-type ref-type})
+
+
+;; Define a protocol around splitting out fact datums and attribute
+;; datums
+
+(defprotocol IFactStore
+  (fact-partition [this] "Entity related facts")
+  (attr-partition [this]  "Attribute (key) metadata"))
+
+(defrecord FactStore [fact-partition attr-partition]
+  IFactStore
+  (fact-partition [this] fact-partition)
+  (attr-partition [this] attr-partition))
+
+(defn- filter-meta
   "Find all facts that are not attribute meta data"
   [facts]
-  (filter (fn [[e a v]] (not (#{:coll :ref :val} v))) facts))
+  (filter (fn [[e a v]] (not (attr-types v))) facts))
 
-(defn filter-not-meta
+(defn- filter-not-meta
   "Find all facts that are attribute metadata"
   [facts]
-  (filter (fn [[e a v]] (#{:coll :ref :val} v)) facts))
+  (filter (fn [[e a v]] (attr-types v)) facts))
 
 (defn q
   "Query against all non-meta facts.  Same arguments as
-   datomic.api/q and should be transparent to the end user"
+   datomic.api/q and should be transparent to the end user.
+   Note we can query across our map and a datomic DB here if
+   we pass the datomic DB as an additional param"
   [query facts & params]
-  (apply d/q (concat [query (filter-meta facts)] params)))
+  {:pre [(satisfies? IFactStore facts)]}
+  (apply d/q (concat [query (fact-partition facts)] params)))
 
 (defn meta-q
   "Query against all attribute meta data facts. Same notes
    as q"
   [query facts & params]
-  (apply d/q (concat [query (filter-not-meta facts)] params)))
+  {:pre [(satisfies? IFactStore facts)]}
+  (apply d/q (concat [query (attr-partition facts)] params)))
 
 
 ;; Functions for converting maps to fact tuples
@@ -40,10 +65,10 @@
   "Handles converting collections to facts, including collections
    of maps"
   [id k coll]
-  (->> (map (fn [k v] [k v]) (take (count coll) (repeat k)) coll)
+  (->> (map (fn [v] [k v]) coll)
        (map (partial map-entry->fact true id))
        (reduce concat)
-       (cons [id k :coll])))
+       (cons [id k coll-type])))
 
 (defn map-entry->fact
   "Generate a fact given an id and [key value] tuple.
@@ -53,7 +78,7 @@
          (sequential? v) (coll->facts id k v)
          (set? v) (coll->facts id k v)
          is-child? [[id k v]]
-         v [[id k v] [id k :val]]
+         v [[id k v] [id k val-type]]
          true nil))
   ([id [k v]]
    (map-entry->fact nil id [k v])))
@@ -78,7 +103,7 @@
           (mapcat (partial map-entry->fact id))
           (filter identity)
           (cons [parent key id])
-          (cons [parent key :ref])))))
+          (cons [parent key ref-type])))))
 
 
 (defn maps->facts
@@ -88,10 +113,9 @@
   (set (mapcat map->facts ms)))
 
 
-(defn facts
+(defn- facts*
   "Given a map or a list of maps, generate a table of facts"
   [m]
-  {:pre [(or (map? m) (sequential? m))]}
   (cond
     (map? m) (map->facts m)
     (sequential? m) (maps->facts m)
@@ -100,10 +124,15 @@
                    {:causes #{:unsupported-type}
                     :type (type m)}))))
 
+(defn facts
+  [m]
+  (let [facts (facts* m)]
+    (FactStore. (filter-meta facts) (filter-not-meta facts))))
+
 ;; Functions for retrieving entities by their generated ID
 
 (defn attr-meta
-  "Find the attributes meta data => (:coll | :ref | :val)"
+  "Find the attributes meta data => (::coll | ::ref | ::val)"
   [facts id attr]
   (meta-q '[:find ?type .
             :in $ ?id ?attr
@@ -132,10 +161,10 @@
       (if-let [[a v] (first datums)]
         (let [type (attr-meta facts id a)]
           (recur (rest datums)
-                 (cond
-                   (= type :coll) (update-in out [a] conj (or (entity facts v) v))
-                   (= type :ref) (assoc out a (entity facts v))
-                   :else (assoc out a v))))
+                 (condp = type
+                   coll-type (update-in out [a] conj (or (entity facts v) v))
+                   ref-type (assoc out a (entity facts v))
+                   (assoc out a v))))
         out))))
 
 (defn entity
