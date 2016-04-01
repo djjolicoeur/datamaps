a2ce7d26ae905349768565632e3a1a84f95af952
Daniel Jolicoeur <djjolicoeur@gmail.com>
2016-04-01 08:58:42 -0400

    update deps

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
-(defproject datamaps "0.1.0-SNAPSHOT"
+(defproject datamaps "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[datascript "0.15.0"]]
   :profiles {:dev {:plugins [[lein-kibit "0.1.2"]]}
-             :provided {:dependencies
-                        [[org.clojure/clojure "1.7.0"]
-                         [datascript "0.15.0"]]}})
-;;[com.datomic/datomic-free "0.9.5350"]
+             :provided {:dependencies [[org.clojure/clojure "1.8.0"]]}})
