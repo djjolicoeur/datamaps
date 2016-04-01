a3e5eb2fec2d1d61f59a8bc348952bfa39e16778
Daniel Jolicoeur <djjolicoeur@gmail.com>
2016-04-01 00:48:00 -0400

    add support for pull API.  needs testing

--- src/datamaps/query.clj
+++ src/datamaps/query.clj
@@ -1,9 +1,78 @@
 (ns datamaps.query
   (:require [datascript.query :as dq]
             [datascript.parser :as dp]
-            [datascript.lru :as lru]))
+            [datascript.pull-parser :as dpp]
+            [datascript.lru :as lru]
+            [datamaps.facts :as df]
+            [datamaps.pull :as dpa])
+  (:import [datascript.parser BindColl BindIgnore BindScalar BindTuple
+            Constant FindColl FindRel FindScalar FindTuple PlainSymbol
+                                RulesVar SrcVar Variable]))
 
 (def ^:private lru-cache-size 100)
+
+(defn rel-with-attr [context sym]
+  (some #(when (contains? (:attrs %) sym) %) (:rels context)))
+
+(defn context-resolve-val [context sym]
+  (when-let [rel (rel-with-attr context sym)]
+    (when-let [tuple (first (:tuples rel))]
+      (get tuple ((:attrs rel) sym)))))
+
+
+(defprotocol IContextResolve
+  (-context-resolve [var context]))
+
+(extend-protocol IContextResolve
+  Variable
+  (-context-resolve [var context]
+    (context-resolve-val context (.-symbol var)))
+  SrcVar
+  (-context-resolve [var context]
+    (get-in context [:sources (.-symbol var)]))
+  PlainSymbol
+  (-context-resolve [var _]
+    (get dq/built-in-aggregates (.-symbol var)))
+  Constant
+  (-context-resolve [var _]
+        (.-value var)))
+
+(defn resolve-pull-source [var context]
+  (get-in context [:pull-sources (.-symbol var)]))
+
+(defn pull [find-elements context resultset]
+  (let [resolved (for [find find-elements]
+                   (when (dp/pull? find)
+                     [(resolve-pull-source (:source find) context)
+                      (dpp/parse-pull
+                       (-context-resolve (:pattern find) context))]))]
+    (for [tuple resultset]
+      (mapv (fn [env el]
+              (if env
+                (let [[src spec] env]
+                  (dpa/pull-spec src spec [el] false))
+                el))
+            resolved
+            tuple))))
+
+
+
+(defn resolve-in [context [binding value]]
+  (cond
+    (and (instance? BindScalar binding)
+         (instance? SrcVar (:variable binding)))
+    (-> context
+        (update-in [:sources]
+                   assoc (get-in binding [:variable :symbol]) (df/fact-partition value))
+        (update-in [:pull-sources] assoc (get-in binding [:variable :symbol]) value))
+    (and (instance? BindScalar binding)
+         (instance? RulesVar (:variable binding)))
+    (assoc context :rules (dq/parse-rules value))
+    :else
+    (update-in context [:rels] conj (dq/in->rel binding value))))
+
+(defn resolve-ins [context bindings values]
+    (reduce resolve-in context (zipmap bindings values)))
 
 (defn collect
   "Override datascript's collect fn to not
@@ -42,7 +111,7 @@
                         (sequential? q) dp/query->map)
         wheres        (:where q)
         context       (-> (datascript.query.Context. [] {} {})
-                          (dq/resolve-ins (:in parsed-q) inputs))
+                          (resolve-ins (:in parsed-q) inputs))
         results       (-> context
                           (dq/-q wheres)
                           (collect all-vars))]
@@ -51,6 +120,6 @@
       (mapv #(vec (subvec % 0 result-arity)))
       (some dp/aggregate? find-elements)
       (dq/aggregate find-elements context)
-      ;;(some dp/pull? find-elements)  ;;TODO: integrate pull
-      ;;(dq/pull find-elements context)
+      (some dp/pull? find-elements)
+      (pull find-elements context)
       true (dq/-post-process find))))
