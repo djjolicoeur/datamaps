3ae8cc2c341c57725c1b76d18888ae06aa90b340
Daniel Jolicoeur <djjolicoeur@gmail.com>
2025-09-03 16:05:03 -0400

    Update datascript dependency

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [datascript "0.15.0"]]
+                 [datascript "1.5.3"]]
   :profiles {:dev {:plugins [[lein-kibit "0.1.2"]]
                    :source-paths ["dev"]}
              :provided {:dependencies [[com.datomic/datomic-free "0.9.5350"]]}})
